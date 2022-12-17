33602d3cc7059de8b18694e621de17fb0c054430
Maryor-z <88011448+Maryor-z@users.noreply.github.com>
2022-12-17 09:26:41 +0100

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,16 @@
-package PACKAGE_NAME;public class Main {
+public class Main {
+    public static void main(String[] args) {
+//        Player player = new Player();
+//
+//        player.fullname = "may";
+//        player.health = 20;
+//        player.weapon = "Sword";
+//
+//        int damage = 10;
+//        player.loseHealth(damage);
+//        System.out.println(player.healthRemaining());
+
+        EnhancedPlayer tim = new EnhancedPlayer("Tim");
+        System.out.println("initial heath " + tim.healthRemaining());
+    }
 }
