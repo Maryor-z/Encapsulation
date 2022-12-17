36a1ae56d19ba81bc9969699848862e2888927f5
Maryor-z <88011448+Maryor-z@users.noreply.github.com>
2022-12-17 09:27:26 +0100

    Update Player.java

--- src/Player.java
+++ src/Player.java
@@ -1,2 +1,26 @@
-package PACKAGE_NAME;public class Player {
+public class Player {
+
+    public String fullname;
+    public int health;
+    public String weapon;
+
+    public void loseHealth (int damage) {
+
+        health -= damage;
+        if (health <= 0) {
+            System.out.println("Player has been knocked out");
+        }
+    }
+
+    public int healthRemaining(){
+        return health;
+    }
+
+    public void restoreHealth(int extraHealth) {
+        health = health + extraHealth;
+        if (health < 100) {
+            System.out.println("Player restored to 100%");
+            health = 100;
+        }
+    }
 }
